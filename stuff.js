db4a07d6fe7ad8a4882643b27cd49aac2f7b6933
MatthewJamesJenks <Jenm1_16@uni.worc.ac.uk>
2017-10-16 14:49:26 +0100

    Made display functions and attempting storing locally

--- stuff.js
+++ stuff.js
@@ -15,12 +15,17 @@ function displaySet1()
 {
   selectedKey = window.localStorage.getItem(key1);
   selectedValue = window.localStorage.getItem(value1);
-  alert("Key: " + selectedKey + "Value: " + selectedValue);
+  displayStuff();
 }
 
 function displaySet2()
 {
   selectedKey = window.localStorage.getItem(key2);
   selectedValue = window.localStorage.getItem(value2);
+  displayStuff();
+}
+
+function displayStuff()
+{
   alert("Key: " + selectedKey + "Value: " + selectedKey);
 }
