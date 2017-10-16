a6ca827bed5e1e4763840d828974571753d575e9
MatthewJamesJenks <Jenm1_16@uni.worc.ac.uk>
2017-10-16 14:35:05 +0100

    Added event listeners

--- stuff.js
+++ stuff.js
@@ -1,8 +1,14 @@
 var key = "Phone";
 var value = "Galaxy A3";
 
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function displayAlert()
 {
   alert("Key: " + key + "Value: " + value);
+}
+
+function onDeviceReady()
+{
+  displayAlert();
 }
